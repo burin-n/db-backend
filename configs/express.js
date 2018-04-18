5e01e87db68b1c7c91dd5931f511dba51358a708
Unknown <burin010n@gmail.com>
2018-04-18 15:56:25 +0700

    cors

--- configs/express.js
+++ configs/express.js
@@ -3,9 +3,11 @@ module.exports = () => {
   let bodyParser = require('body-parser');
   let morgan = require('morgan');
   let app = require('express')();
+	let cors = require('cors');
 
   app.use(morgan(':remote-addr :remote-user [:date[clf]] HTTP/:http-version" :method :url :status :res[content-length] - :response-time ms :user-agent'));
 
+	app.use(cors());
   app.use(bodyParser.urlencoded({
 		limits: '10mb',
 		extended: true
