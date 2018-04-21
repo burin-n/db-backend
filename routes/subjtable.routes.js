2f4b63c8f8b2b218fb04fcfe3275704c1e22d4b5
Pol <pol.surakit@gmail.com>
2018-04-21 14:11:06 +0700

    edit route of get table

--- routes/subjtable.routes.js
+++ routes/subjtable.routes.js
@@ -2,7 +2,9 @@ module.exports = (app) => {
 	const request = require('../controllers/subjtable.controllers');
 	const passport = require('passport');
 
-  app.post('/subjtable',passport.authenticate('bearer', { session: false }),request.getTable);
+  app.post('/table/subj',passport.authenticate('bearer', { session: false }),request.getSubjTable);
+  app.post('/table/mid',passport.authenticate('bearer', { session: false }),request.getMidTable);
+  app.post('/table/fin',passport.authenticate('bearer', { session: false }),request.getFinTable);
   //app.delete('/request',passport.authenticate('bearer', { session: false }),request.deleteRequest);
   //app.post('/request/result',passport.authenticate('bearer', { session: false }),request.getRequestResult);
 
